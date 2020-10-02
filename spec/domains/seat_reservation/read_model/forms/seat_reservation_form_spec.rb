609b25790179dffe6d83ac776d7db34fc604d915
Evgenii Danilov <jmelkor@rabmler.ru>
2020-10-02 20:08:35 +0300

    Add missing tests

---
+++ spec/domains/seat_reservation/read_model/forms/seat_reservation_form_spec.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe SeatReservation::ReadModel::Forms::SeatReservationForm do
+  subject { described_class.new(params) }
+
+  context 'with valid params' do
+    let(:params) { { number: '1' } }
+
+    it 'returns form object' do
+      expect(subject).to have_attributes(number: 1)
+    end
+  end
+
+  context 'with invalid params' do
+    let(:params) { { number: 'invalid' } }
+
+    it 'returns form object' do
+      expect { subject }.to raise_error(Dry::Struct::Error)
+    end
+  end
+end
