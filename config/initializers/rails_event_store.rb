5b781b206cc5e970e6f8b670db1665ff671f1699
Evgenii Danilov <evgenii.danilov@smartpension.co.uk>
2021-01-03 15:39:34 +0300

    Clean up the code

--- config/initializers/rails_event_store.rb
+++ config/initializers/rails_event_store.rb
@@ -6,7 +6,6 @@ require 'arkency/command_bus'
 
 Rails.configuration.to_prepare do
   Rails.configuration.event_store = RailsEventStore::Client.new
-  Rails.configuration.command_bus = Arkency::CommandBus.new
 
   AggregateRoot.configure do |config|
     config.default_event_store = Rails.configuration.event_store
--- lib/publisher.rb
+++ lib/publisher.rb
@@ -1,20 +1,9 @@
 # frozen_string_literal: true
 
 class Publisher
+
   def self.broadcast(event, stream_name)
-    new(event, stream_name).broadcast
-  end
-
-  def initialize(event, stream_name)
-    @event = event
-    @stream_name = stream_name
-  end
-
-  def broadcast
     Rails.configuration.event_store.publish(event, stream_name: stream_name)
   end
 
-  private
-
-  attr_reader :event, :stream_name
 end
